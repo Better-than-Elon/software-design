0c81e6407dacf7b72e63bafe6f8f305586bec515
Better-than-Elon <arkadyartemenko@gmail.com>
2022-11-07 02:09:15 +0300

    refactor GetProducts

--- refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/GetProductsServlet.java
+++ refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/GetProductsServlet.java
@@ -1,38 +1,29 @@
 package ru.akirakozov.sd.refactoring.servlet;
 
+import ru.akirakozov.sd.refactoring.database.Dao;
+import ru.akirakozov.sd.refactoring.printer.ResponsePrinter;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 /**
  * @author akirakozov
  */
 public class GetProductsServlet extends HttpServlet {
+    private final Dao dao;
+
+    public GetProductsServlet(Dao dao) {
+        this.dao = dao;
+    }
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+
+        ResponsePrinter printer = new ResponsePrinter();
+
         try {
-            try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                Statement stmt = c.createStatement();
-                ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUCT");
-                response.getWriter().println("<html><body>");
-
-                while (rs.next()) {
-                    String  name = rs.getString("name");
-                    int price  = rs.getInt("price");
-                    response.getWriter().println(name + "\t" + price + "</br>");
-                }
-                response.getWriter().println("</body></html>");
-
-                rs.close();
-                stmt.close();
-            }
-
+            printer.printResponseProducts(response, "Get products", dao.getAll());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
