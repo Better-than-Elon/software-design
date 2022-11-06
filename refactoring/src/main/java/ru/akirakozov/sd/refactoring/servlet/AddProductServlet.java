e43318ab239095e30c2c7240d919647ae82bd853
Better-than-Elon <arkadyartemenko@gmail.com>
2022-11-07 02:08:43 +0300

    refactor AddProduct

--- refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/AddProductServlet.java
+++ refactoring/src/main/java/ru/akirakozov/sd/refactoring/servlet/AddProductServlet.java
@@ -1,17 +1,22 @@
 package ru.akirakozov.sd.refactoring.servlet;
 
+import ru.akirakozov.sd.refactoring.database.Dao;
+import ru.akirakozov.sd.refactoring.model.Product;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
 
 /**
  * @author akirakozov
  */
 public class AddProductServlet extends HttpServlet {
+    private final Dao dao;
+
+    public AddProductServlet(Dao dao){
+        this.dao = dao;
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -19,13 +24,7 @@ public class AddProductServlet extends HttpServlet {
         long price = Long.parseLong(request.getParameter("price"));
 
         try {
-            try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
-                String sql = "INSERT INTO PRODUCT " +
-                        "(NAME, PRICE) VALUES (\"" + name + "\"," + price + ")";
-                Statement stmt = c.createStatement();
-                stmt.executeUpdate(sql);
-                stmt.close();
-            }
+            dao.insertProduct(new Product(name, price));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
